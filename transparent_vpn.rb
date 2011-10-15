e0f6ea6421ee983601f353d9f3b3a0421864e072
Eike Siewertsen <e.siew@londeroth.org>
2011-10-15 02:11:45 +0200

    Fix transparent_vpn.rb indentation

--- transparent_vpn.rb
+++ transparent_vpn.rb
@@ -14,7 +14,8 @@ def xe(*args)
 end
 
 if ENV.include? 'script_type' then
-	overwrite_ip = ENV['CFG_LOCAL_IP'] != ENV['ifconfig_local'] and ENV['CFG_LOCAL_IP'] != "default"
+	overwrite_ip = ENV['CFG_LOCAL_IP'] != ENV['ifconfig_local']
+	               and ENV['CFG_LOCAL_IP'] != "default"
 	local_ip = overwrite_ip ? ENV['CFG_LOCAL_IP'] : ENV['ifconfig_local']
 		
 	table = ENV['CFG_ROUTE_TABLE']
@@ -26,15 +27,16 @@ if ENV.include? 'script_type' then
 	ip = "/sbin/ip"
 	iptables = "/sbin/iptables"
 
-	iptables_args = ["POSTROUTING", "-t", "nat", "-s", ENV['CFG_LOCAL_IP'], "-o", device, "-j", "SNAT", "--to-source", ENV['ifconfig_local']]
+	iptables_args = ["POSTROUTING", "-t", "nat", "-s", ENV['CFG_LOCAL_IP'], "-o",
+	                 device, "-j", "SNAT", "--to-source", ENV['ifconfig_local']]
 
 	if type == "up" then
 		xe(ifconfig, device, local_ip, "pointopoint", ENV['ifconfig_remote'],
-			"mtu", ENV['tun_mtu'])
+		   "mtu", ENV['tun_mtu'])
 	elsif type == "route-up" then
 		xe(ip, "rule", "add", "from", local_ip, "table", table)
 		xe(ip, "route", "add", "default", "via", ENV['ifconfig_remote'],
-		      "dev", device, "table", table)
+		   "dev", device, "table", table)
 		
 		# register iptables rule if we're picking our own ip
 		xe(iptables, "-A", *iptables_args) if overwrite_ip
@@ -44,7 +46,7 @@ if ENV.include? 'script_type' then
 		socat_dest = ENV['CFG_SOCAT_DEST']
 		if socat_port and socat_dest
 			x("socat", "-d", "-v", "-v", "TCP4-LISTEN:#{socat_port},fork,reuseaddr",
-        "TCP4:#{socat_dest},bind=#{local_ip}")
+			  "TCP4:#{socat_dest},bind=#{local_ip}")
 		end
 	elsif type == "down" then
 		xe(ifconfig, device, "0.0.0.0")
@@ -67,32 +69,33 @@ else
 		opts.banner = "Usage: #{__FILE__} [options]"
 		
 		opts.on("-c", "--config CONFIG",
-						"The OpenVPN CONFIG file to use") do |config|
+		        "The OpenVPN CONFIG file to use") do |config|
 			options.config = config
 		end
 		
 		opts.on("-d", "--dev [DEVICE]", "The TUN device for OpenVPN to use",
-					  "default is to create a temporary device") do |dev|
+		        "default is to create a temporary device") do |dev|
 			options.device = dev
 		end
 		
 		opts.on("-b", "--bind [IP]", "The local IP to use",
-					 "default is to use the one assigned by the server") do |ip|
+		        "default is to use the one assigned by the server") do |ip|
 			options.ip = ip
 		end
 		
 		opts.on("-t", "--table TABLE",
-					 "The routing table to use for setting up the default route",
-					 "has to be created beforehand") do |table|
+		        "The routing table to use for setting up the default route",
+		        "has to be created beforehand") do |table|
 			options.table = table
 		end
 		
 		opts.on("-s", "--socat [DEST]", "Setup a socat TCP redirect through the " +
-																		"VPN to DEST") do |dest|
+		        "VPN to DEST") do |dest|
 			options.socat_dest = dest
 		end
 		
-		opts.on("-p", "--port [PORT]", Integer, "The port for socat to listen on") do |port|
+		opts.on("-p", "--port [PORT]", Integer,
+		        "The port for socat to listen on") do |port|
 			options.socat_port = port
 		end
 		
@@ -116,22 +119,22 @@ else
 		["CFG_SOCAT_DEST", options.socat_dest]
 	]
 
-  args = ["openvpn",
-	  "--config", options.config,
-	  "--script-security", "2",
-	  "--route-nopull",
-	  "--ifconfig-noexec",
-	  "--up", __FILE__,
-	  "--down", __FILE__,
-	  "--route-up", __FILE__]
+	args = ["openvpn",
+	        "--config", options.config,
+	        "--script-security", "2",
+	        "--route-nopull",
+	        "--ifconfig-noexec",
+	        "--up", __FILE__,
+	        "--down", __FILE__,
+	        "--route-up", __FILE__]
 
-  args += env_args.find_all do |env|
-		not env[1].nil?
+	args += env_args.find_all do |env|
+	  not env[1].nil?
 	end.map do |env|
 	  ["--setenv", env[0].to_s, env[1].to_s]
 	end.flatten
 
-  args += ["--dev", options.device] if options.device
+	args += ["--dev", options.device] if options.device
 	
   system(*args)
 end
